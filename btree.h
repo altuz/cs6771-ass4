df5ea36bb643aa32626fa413e72cf00fe5d73182
altuz.nicholas <altuz.nicholas@gmail.com>
2017-10-05 09:22:01 +1100

    new rep

--- btree.h
+++ btree.h
@@ -2,8 +2,8 @@
  * The btree is a linked structure which operates much like
  * a binary search tree, save the fact that multiple client
  * elements are stored in a single node.    Whereas a single element
- * would partition the tree into two ordered subtrees, a node 
- * that stores m client elements partition the tree 
+ * would partition the tree into two ordered subtrees, a node
+ * that stores m client elements partition the tree
  * into m + 1 sorted subtrees.
  */
 
@@ -22,34 +22,53 @@
 // we do this to avoid compiler errors about non-template friends
 // what do we do, remember? :)
 
-template <typename T> 
+template <typename T>
 class btree {
 private:
     // The details of your implementation go here
-    unsigned int _size;
     // TODO: Combine vals and trees?
     //       In total you will have n + 1 sub-trees.
     //       Each sub-tree has a value, and their child have to be strictly less than that value
     //              but higher than the immediate preceding sub-tree
     //       Right most subtree will be tricky, but never include the (n + 1)th value at any point, since that is imaginary.
-    std::vector<T> _childVals;
-    std::vector<std::shared_ptr<btree<T>>> _childTrees;
+    class bnode {
+    public:
+        unsigned int _size;
+        std::vector<T> _childVals;
+        std::vector<std::shared_ptr<bnode>> _childTrees;
+        std::shared_ptr<bnode> _parent;
+
+        bnode(size_t maxNodeElems = 40) : _size(maxNodeElems) {
+            _childVals.reserve(_size);
+            // There are n-1 sub-trees in between the n values.
+            // There are one sub-tree in each end
+            // Thus in total we have n + 1 sub trees
+            _childTrees.reserve(_size + 1);
+            _parent = nullptr;
+        };
+
+        std::vector<T> bfs() const {
+            // Copy all values
+            std::cout << "Size is :" << _childVals.size() << "\n";
+            std::vector<T> res(_childVals);
+            for(auto child : _childTrees) {
+                auto c_res = (*child).bfs();
+                std::move(c_res.begin(), c_res.end(), std::back_inserter(res));
+            }
+            return res;
+        }
+    };
+
+    std::shared_ptr<bnode> _root;
     /**
      * Goes through the tree using level-order traversal a.k.a bfs
      * Returns a vector of the values
      */
-    std::vector<T> bfs() const {
-        // Copy all values
-        std::vector<T> res(_childVals);
-        for(auto child : _childTrees) {
-            auto c_res = (**child).bfs();
-            std::move(c_res.begin(), c_res.end(), std::back_inserter(res));
-        }
-        return res;
-    }
 public:
     /** Hmm, need some iterator typedefs here... friends? **/
- 
+    friend class btree_iterator<T>;
+    typedef btree_iterator<T> iterator;
+    typedef const_btree_iterator<T> const_iterator;
     /**
      * Constructs an empty btree.    Note that
      * the elements stored in your btree must
@@ -59,54 +78,46 @@ public:
      * relative to each other by implementing operator<
      * and operator==. (These are already implemented on
      * behalf of all built-ins: ints, doubles, strings, etc.)
-     * 
+     *
      * @param maxNodeElems the maximum number of elements
      *                that can be stored in each B-Tree node
      */
-    btree(size_t maxNodeElems = 40) : _size(maxNodeElems) {
-        _childVals.reserve(size);
-        // There are n-1 sub-trees in between the n values.
-        // There are one sub-tree in each end
-        // Thus in total we have n + 1 sub trees
-        _childTrees.reserve(size + 1);
-    };
+    btree(size_t maxNodeElems = 40) : _root(std::make_shared<bnode>(maxNodeElems)) {};
 
     /**
      * The copy constructor and    assignment operator.
      * They allow us to pass around B-Trees by value.
      * Although these operations are likely to be expensive
      * they make for an interesting programming exercise.
-     * Implement these operations using value semantics and 
+     * Implement these operations using value semantics and
      * make sure they do not leak memory.
      */
 
-    /** 
+    /**
      * Copy constructor
      * Creates a new B-Tree as a copy of original.
      *
      * @param original a const lvalue reference to a B-Tree object
      */
-    btree(const btree<T>& original) : btree(original.size) {
+    btree(const btree<T>& original) : btree(original._root->_size) {
         // Get a list of all elements in original tree
         // Insert them one by one
-        auto all_values = original.bfs();
+        auto all_values = original._root->bfs();
         for (auto i : all_values) {
-            this->insert(*i);
+            (void) i;
+            // this->insert(i);
         }
     };
 
-    /** 
+    /**
      * Move constructor
      * Creates a new B-Tree by "stealing" from original.
      *
      * @param original an rvalue reference to a B-Tree object
      */
-    btree(btree<T>&& original) {
-        std::swap(*this, original);
-    };
-    
-    
-    /** 
+    btree(btree<T>&& original) : _root(std::move(original._root)){};
+
+    /**
      * Copy assignment
      * Replaces the contents of this object with a copy of rhs.
      *
@@ -114,10 +125,11 @@ public:
      */
     btree<T>& operator=(const btree<T>& rhs) {
         auto rhs_copy(rhs);
-        std::swap(*this, rhs_copy);
+        *this = std::move(rhs_copy);
+        return *this;
     };
 
-    /** 
+    /**
      * Move assignment
      * Replaces the contents of this object with the "stolen"
      * contents of original.
@@ -126,7 +138,8 @@ public:
      */
     btree<T>& operator=(btree<T>&& rhs) {
         auto rhs_new(std::move(rhs));
-        std::swap(*this, rhs_new);
+        this->_root = std::move(rhs_new._root);
+        return *this;
     };
 
     /**
@@ -138,46 +151,82 @@ public:
      * @param tree a const reference to a B-Tree object
      * @return a reference to os
      */
-    friend std::ostream& operator<< <T> (std::ostream& os, const btree<T>& tree) {
-        auto vals = tree.bfs();
+    friend std::ostream& operator<< (std::ostream& os, const btree<T>& tree) {
+        auto vals = tree._root->bfs();
         for (auto i = vals.cbegin(); i != vals.cend(); ++i) {
             if (i != vals.cbegin())
                 os << ' ';
-            os << i;
+            os << *i;
         }
         return os;
     };
 
     /**
      * The following can go here
-     * -- begin() 
-     * -- end() 
-     * -- rbegin() 
-     * -- rend() 
-     * -- cbegin() 
-     * -- cend() 
-     * -- crbegin() 
-     * -- crend() 
+     * -- begin()
+     * -- end()
+     * -- rbegin()
+     * -- rend()
+     * -- cbegin()
+     * -- cend()
+     * -- crbegin()
+     * -- crend()
      */
-    
+     iterator end() {
+        return findEnd(_root);
+     };
+
+     iterator findEnd(std::shared_ptr<bnode> node) {
+         auto dist = std::distance(std::begin(node->_childVals), std::end(node->_childVals));
+         if(dist == node->_size) {
+             auto final_subtree = node->_childTrees[node->_size];
+             if(final_subtree)
+                return findEnd(final_subtree);
+         }
+         return iterator(std::end(node->_childVals), node);
+     }
     /**
-        * Returns an iterator to the matching element, or whatever 
-        * the non-const end() returns if the element could 
-        * not be found.    
+        * Returns an iterator to the matching element, or whatever
+        * the non-const end() returns if the element could
+        * not be found.
         *
         * @param elem the client element we are trying to match.    The elem,
         *                if an instance of a true class, relies on the operator< and
-        *                and operator== methods to compare elem to elements already 
+        *                and operator== methods to compare elem to elements already
         *                in the btree.    You must ensure that your class implements
         *                these things, else code making use of btree<T>::find will
         *                not compile.
         * @return an iterator to the matching element, or whatever the
         *                 non-const end() returns if no such match was ever found.
         */
-    iterator find(const T& elem);
-        
+    iterator find(const T& elem) {
+        return find(elem, _root);
+    };
+
+    iterator find(const T& elem, std::shared_ptr<bnode> node) {
+        if (node) {
+            auto c_nodes = node->_childVals;
+            auto c_trees = node->_childTrees;
+            auto lower_bound = std::lower_bound(c_nodes.begin(), c_nodes.end(), elem);
+            // check if lower bound is end of iterator
+            if(lower_bound != c_nodes.end()) {
+                // check value
+                if(*lower_bound == elem) {
+                    // elem is found
+                    return iterator(lower_bound, node);
+                }
+                // elem not found
+                auto subtree_idx = std::distance(c_nodes.begin(), lower_bound) + 1;
+                auto subtree     = c_trees[subtree_idx];
+                if (subtree) {
+                    return find(elem, subtree);
+                }
+            }
+        }
+        return end();
+    };
     /**
-        * Identical in functionality to the non-const version of find, 
+        * Identical in functionality to the non-const version of find,
         * save the fact that what's pointed to by the returned iterator
         * is deemed as const and immutable.
         *
@@ -186,7 +235,7 @@ public:
         *                 const end() returns if no such match was ever found.
         */
     const_iterator find(const T& elem) const;
-            
+
     /**
         * Operation which inserts the specified element
         * into the btree if a matching element isn't already
@@ -194,35 +243,44 @@ public:
         * to be inserted, the size of the btree is effectively
         * increases by one, and the pair that gets returned contains
         * an iterator to the inserted element and true in its first and
-        * second fields.    
+        * second fields.
         *
         * If a matching element already exists in the btree, nothing
-        * is added at all, and the size of the btree stays the same.    The 
+        * is added at all, and the size of the btree stays the same.    The
         * returned pair still returns an iterator to the matching element, but
         * the second field of the returned pair will store false.    This
         * second value can be checked to after an insertion to decide whether
         * or not the btree got bigger.
         *
-        * The insert method makes use of T's zero-arg constructor and 
-        * operator= method, and if these things aren't available, 
+        * The insert method makes use of T's zero-arg constructor and
+        * operator= method, and if these things aren't available,
         * then the call to btree<T>::insert will not compile.    The implementation
         * also makes use of the class's operator== and operator< as well.
         *
         * @param elem the element to be inserted.
         * @return a pair whose first field is an iterator positioned at
-        *                 the matching element in the btree, and whose second field 
-        *                 stores true if and only if the element needed to be added 
+        *                 the matching element in the btree, and whose second field
+        *                 stores true if and only if the element needed to be added
         *                 because no matching element was there prior to the insert call.
         */
-    std::pair<iterator, bool> insert(const T& elem);
+    std::pair<iterator, bool> insert(const T& elem) {
+        auto res = find(elem);
+        if(res != end()) {
+            return std::make_pair<iterator, bool>(res, false);
+        }
 
+    };
+
+    std::pair<iterator, bool> insert(const T& elem, std::shared_ptr<bnode> node) {
+
+    }
     /**
         * Disposes of all internal resources, which includes
         * the disposal of any client objects previously
-        * inserted using the insert operation. 
+        * inserted using the insert operation.
         * Check that your implementation does not leak memory!
         */
-    ~btree();
+    ~btree(){};
 };
 
 #endif
--- btree_iterator.h
+++ btree_iterator.h
@@ -2,13 +2,42 @@
 #define BTREE_ITERATOR_H
 
 #include <iterator>
-
+#include <memory>
+#include <vector>
 /**
  * You MUST implement the btree iterators as (an) external class(es) in this file.
  * Failure to do so will result in a total mark of 0 for this deliverable.
  **/
-
+template <typename T> class btree;
 // iterator related interface stuff here; would be nice if you called your
 // iterator class btree_iterator (and possibly const_btree_iterator)
+template <typename T> class btree_iterator {
+public:
+    typedef std::ptrdiff_t                     difference_type;
+    typedef std::bidirectional_iterator_tag    iterator_category;
+    typedef T                                  value_type;
+    typedef T*                                 pointer;
+    typedef T&                                 reference;
 
+    typedef typename btree<T>::bnode tree_node;
+    typedef typename std::vector<T>::iterator location;
+    reference operator*() const;
+    pointer operator->() const { return &(operator*()); }
+    btree_iterator& operator++();
+    bool operator==(const btree_iterator& other) const {
+        return (_currNode == other._currNode && _currTree == other._currTree);
+    };
+    bool operator!=(const btree_iterator& other) const { return !operator==(other); }
+    btree_iterator(location currNode, std::shared_ptr<tree_node> currTree) :
+    _currNode(currNode), _currTree(currTree) {}
+
+private:
+    // positioning in subtree
+    location _currNode;
+    // subtree
+    std::shared_ptr<tree_node> _currTree;
+};
+
+template <typename T> class const_btree_iterator {
+};
 #endif
